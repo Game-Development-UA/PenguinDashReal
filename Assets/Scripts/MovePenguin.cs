816c7747f135baab04f1f2df865a5bf1305ea78a
Zach Riblett <zriblett@email.arizona.edu>
2019-03-21 17:00:04 -0700

    added jumping ability

--- Assets/Scripts/MovePenguin.cs
+++ Assets/Scripts/MovePenguin.cs
@@ -5,15 +5,22 @@ using UnityEngine;
 public class MovePenguin : MonoBehaviour
 {
 	public float speed;
+    public float jumpForce;
+    public Rigidbody body;
+    float timer = 0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        timer = Time.time;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0f, 0f);
+        body.AddForce(Input.GetAxis("Horizontal") * speed, 0f, 0f);
+        if (Input.GetKeyDown(KeyCode.UpArrow) && (Time.time - timer > 1.0f)){
+                body.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
+                timer = Time.time;
+        }
     }
 }
