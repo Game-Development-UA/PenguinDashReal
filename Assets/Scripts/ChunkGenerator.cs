ca3ff883b61cf63dfe673d6216557c19daad9fd6
Zach Riblett <zriblett@email.arizona.edu>
2019-03-21 18:42:24 -0700

    added platform generation

---
+++ Assets/Scripts/ChunkGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChunkGenerator : MonoBehaviour
+{
+	public GameObject nextChunk;
+	public Transform nextChunkSpawnLoc;
+
+	void OnTriggerEnter( Collider otherCol ) {
+		GameObject newChunk = Instantiate<GameObject>( nextChunk );
+		newChunk.transform.position = nextChunkSpawnLoc.position;
+		newChunk.transform.rotation = nextChunkSpawnLoc.rotation;
+	}
+}
---
+++ Assets/Scripts/DestroyAfter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfter : MonoBehaviour
+{
+    // Start is called before the first frame update
+	public int afterTime;
+
+    void Start()
+    {
+       Destroy(this.gameObject, afterTime); 
+    }
+}
