6b7c400b4f513fcaeb3fcf329f7fc8502a08b443
Drew Castalia <dcastalia@email.arizona.edu>
2019-03-29 11:19:18 -0700

    * Refactored MovePenguin to be solely responsible for penguin movement. * Removed complicated boolean timers for individual UI event.  Replaced with a single animation "StartCounter". * Moved collision logic to obstacles. * Added gutter to platform chunk that detects failure state instead of constantly calculating it every frame in the player script. * Moved UI elements onto discrete canvases so they can be swapped in and out easily.

---
+++ Assets/Scripts/ChangeLabelText.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ChangeLabelText : MonoBehaviour
+{
+	public TextMeshProUGUI label;
+
+	public void ChangeText( string newText ) {
+		label.text = newText;
+	}
+}
---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour
+{
+	public MovePenguin player;
+	public GameObject startUI;
+
+	public void Begin() {
+		player.body.isKinematic = false;
+
+		Destroy( startUI );
+	}
+}
