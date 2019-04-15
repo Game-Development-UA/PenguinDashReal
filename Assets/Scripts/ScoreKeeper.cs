03b8011ffa20ffee6275f86ed283d8f8c666f853
Zach Riblett <zriblett@email.arizona.edu>
2019-04-15 13:26:29 -0700

    added highscore/changed some game mechanics to make it slightly easier

--- Assets/Scripts/ScoreKeeper.cs
+++ Assets/Scripts/ScoreKeeper.cs
@@ -1,19 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class ScoreKeeper : MonoBehaviour
 {
 	public TextMeshProUGUI label;
 	float score;
-
 	public MovePenguin player;
+	public TextMeshProUGUI highscore;
+	public TextMeshProUGUI highscoreLabel;
+	bool newHighscore = false;
+	bool changedLabel = false;
+
+	void Start () {
+		highscore.text = PlayerPrefs.GetInt("HighScore", 0).ToString();
+	}
+
+	public void Reset(){
+		PlayerPrefs.DeleteAll();
+		highscore.text = "0";
+	}
 
 	void FixedUpdate() {
-		if(!player.dead){
+		if(!player.dead && player.started){
 			score += player.body.velocity.z;
-			label.text = ((int)score).ToString();
+			int number = (int)score;
+			label.text = number.ToString();
+
+			if(number > PlayerPrefs.GetInt("HighScore", 0)){
+				PlayerPrefs.SetInt("HighScore", number);
+				newHighscore = true;
+			}
+
+			if(newHighscore && !changedLabel){
+				highscoreLabel.text = "New Highscore! :";
+				changedLabel = true;
+			}
 		}
 	}
 }
--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -1,16 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
 
 public class StartGame : MonoBehaviour
 {
 	public MovePenguin player;
 	public GameObject startUI;
+	public TextMeshProUGUI label;
+	public TextMeshProUGUI score;
+	public GameObject highscore;
+	public GameObject highscoreLabel;
+
 
 	public void Begin() {
+		highscoreLabel.SetActive(false);
+		highscore.SetActive(false);
 		player.body.isKinematic = false;
 		GameObject.Find("TitleText").SetActive(false);
 		Destroy( startUI );
+		score.text = "0";
+		label.text = "Score: ";
 		player.started = true;
 	}
 }
