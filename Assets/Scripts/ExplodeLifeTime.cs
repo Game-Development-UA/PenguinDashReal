03d5b5f2ddbc678fade3698c91677c57661157c2
Zach Riblett <zriblett@email.arizona.edu>
2019-04-16 10:03:16 -0700

    added a lifetime to explosions to avoid unused memory being filled

---
+++ Assets/Scripts/ExplodeLifeTime.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplodeLifeTime : MonoBehaviour
+{
+    public int life;
+    void Start()
+    {
+        Destroy(this.gameObject, life);
+    }
+}
